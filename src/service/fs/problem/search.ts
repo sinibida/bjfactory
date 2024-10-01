25a7137a9db910197cc357563bd258578f7894c7
SPAUPA <40449398+sinibida@users.noreply.github.com>
2024-10-01 13:26:28 +0900

    feat: search default values

--- src/service/fs/problem/search.ts
+++ src/service/fs/problem/search.ts
@@ -1,35 +1,42 @@
-import fs from 'fs/promises'
-import path from 'path';
+import fs from "fs/promises";
+import path from "path";
 
 export function keywordCheck(targetPath: string, keyword: string) {
   return targetPath.startsWith(keyword);
 }
 
-export async function* searchProblemDirectories(
-  {
+export async function* searchProblemDirectories(props: {
+  /**
+   * keyword to serach
+   */
+  keyword: string;
+  /**
+   * directory to search onto.
+   * @default Working directory.
+   */
+  searchSrc?: string;
+  /**
+   * @default false
+   */
+  recursive?: boolean;
+}) {
+  const {
     keyword,
     searchSrc: searchSrcProp,
     recursive,
-  }: {
-    /**
-     * keyword to serach
-     */
-    keyword: string,
-    /**
-     * directory to search onto.
-     * @default Working directory.
-     */
-    searchSrc: string,
-    recursive: boolean,
-  }
-) {
+  }: typeof props = {
+    recursive: false,
+    ...props,
+  };
+
+  const searchSrc =
+    searchSrcProp === undefined ? process.cwd() : path.resolve(searchSrcProp);
+
   if (recursive) {
     throw Error("not implemented");
   }
 
-  const searchSrc = path.resolve(searchSrcProp) ?? process.cwd();
-
-  const iter = await fs.opendir(searchSrc)
+  const iter = await fs.opendir(searchSrc);
   for await (const dir of iter) {
     if (!dir.isDirectory()) continue;
 
