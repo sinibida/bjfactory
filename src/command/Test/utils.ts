10cd301f17ab1f4cd5a139b4a5ac6aae110156a6
SPAUPA <40449398+sinibida@users.noreply.github.com>
2024-10-14 12:16:33 +0900

    feat: no-newline & normalize

--- src/command/Test/utils.ts
+++ src/command/Test/utils.ts
@@ -1,15 +1,24 @@
 import colors from 'colors';
 import { Change } from "diff";
 
+function normalizeDiffWord(word: string) {
+  if (/^[\n\r]+$/.test(word)) {
+    return word.replace(/\n/g, '\\n').replace(/\r/g, '\\r');
+  } 
+
+  return word;
+}
+
 export function printDiff(diff: Change[]) {
   // TODO: Use Logger
   diff.forEach(change => {
+    const normValue = normalizeDiffWord(change.value);
     if (change.added) {
-      process.stdout.write(colors.bgGreen(change.value));
+      process.stdout.write(colors.bgGreen(normValue));
     } else if (change.removed) {
-      process.stdout.write(colors.bgRed(change.value));
+      process.stdout.write(colors.bgRed(normValue));
     } else {
-      process.stdout.write(change.value);
+      process.stdout.write(normValue);
     }
   })
 }
